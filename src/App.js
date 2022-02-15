72897da43c73a27a28c6507f5e75b41fb8b9928d
Lionel Kandekwe <lionelkandekwe@Lionels-MacBook-Pro.local>
2022-02-15 21:08:19 +0200

    User login & logout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
+import React, { useEffect } from "react"
 import Header from "./Layouts/Header"
 import Home from "./components/Home"
 import Checkout from "./components/Checkout"
 import Login from "./components/Login"
 import { Routes, Route } from "react-router-dom"
 import "./App.css"
+import { auth } from "./firebase"
+import { useStateValue } from "./components/StateProvider"
 
 function App() {
+  const [{}, dispatch] = useStateValue()
+
+  useEffect(() => {
+    auth.onAuthStateChanged((authUser) => {
+      console.log("The User is >>> ", authUser)
+      if (authUser) {
+        // user loggedin
+        dispatch({
+          type: "SET_USER",
+          user: authUser,
+        })
+      } else {
+        //user logged out
+        dispatch({
+          type: "SET_USER",
+          user: null,
+        })
+      }
+    })
+  }, [])
   return (
     <div className="app">
       <Routes>
--- src/components/Product.js
+++ src/components/Product.js
@@ -5,8 +5,6 @@ import { useStateValue } from "./StateProvider"
 const Product = ({ id, title, image, price, rating }) => {
   const [{ basket }, dispatch] = useStateValue()
 
-  console.log("This is the basket>>>>>", basket)
-
   const addToBasket = () => {
     // dispatch the item into dataLayer
     dispatch({
