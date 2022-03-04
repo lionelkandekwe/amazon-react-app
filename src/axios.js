b1e96c253dda72e6d434146cbb0cfa55c91a05c0
Lionel Kandekwe <lionelkandekwe@Lionels-MacBook-Pro.local>
2022-03-04 17:53:23 +0200

    Deploy app

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios"
 
 const instance = axios.create({
-  baseURL: "http://localhost:5001/react-app-1cdea/us-central1/api",
+  baseURL: "https://us-central1-react-app-1cdea.cloudfunctions.net/api",
 })
 
 export default instance
+
+//"http://localhost:5001/react-app-1cdea/us-central1/api"
